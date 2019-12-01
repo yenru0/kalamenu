39e84c49f1dd5c2f00ea27e8c07573de6470bdb9
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 22:54:16 +0900

    Health 에러 수정

--- src/yenru0/yrkaier/kalamenu/KalaItemEvent.kt
+++ src/yenru0/yrkaier/kalamenu/KalaItemEvent.kt
@@ -13,18 +13,20 @@ class KalaItemEvent : KalaUtil(), Listener {
     fun whenUseKalaItem(e: PlayerInteractEvent){
         val player = e.player
         if (e.action == Action.RIGHT_CLICK_AIR || e.action == Action.RIGHT_CLICK_BLOCK) {
-            if (findKalaId(player.inventory.itemInMainHand) == KalaItems.Bible.v.kalaId){
-                val healed = Random.nextInt(2, 5).toDouble()
-                if (player.healthScale == player.health) {
-                    player.sendMessage("${ChatColor.GOLD}이미 체력이 충분합니다!")
-                } else if (player.healthScale < player.health + healed) {
-                    player.health = player.healthScale
-                    player.inventory.itemInMainHand.amount -= 1
-                    player.sendMessage("${ChatColor.GOLD}기본 성경책으로 인해 당신은 피가 모두 회복되었습니다!")
-                } else {
-                    player.inventory.itemInMainHand.amount -= 1
-                    player.health += healed
-                    player.sendMessage("${ChatColor.GOLD}기본 성경책으로 인해 당신은 ${ round(healed*100/100) }가 회복되었습니다!")
+            if (e.item != null) {
+                if (findKalaId(e.item!!) == KalaItems.Bible.v.kalaId) {
+                    val healed = Random.nextInt(3, 5)
+                    if (player.healthScale == player.health) {
+                        player.sendMessage("${ChatColor.GOLD}이미 체력이 충분합니다!")
+                    } else if (player.healthScale < player.health + healed) {
+                        player.health = player.healthScale
+                        player.inventory.itemInMainHand.amount -= 1
+                        player.sendMessage("${ChatColor.GOLD}기본 성경책으로 인해 당신은 피가 모두 회복되었습니다!")
+                    } else {
+                        player.inventory.itemInMainHand.amount -= 1
+                        player.health += healed
+                        player.sendMessage("${ChatColor.GOLD}기본 성경책으로 인해 당신은 ${healed}가 회복되었습니다!")
+                    }
                 }
             }
         }
